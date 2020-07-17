5c970e774629b6d900d46b00bc54da2fd4635234
大人 <345111458@qq.com>
2020-07-17 16:28:16 +0800

    添加搜索条件格式化 class

--- README.md
+++ README.md
@@ -9,36 +9,27 @@
 $ composer require large/zhengdada -vvv
 ```
 
-## Usage
 
-TODO
 
-## Contributing
-这一行是测试，在来一次
+## 创建目录用法
 
-创建目录用法
 1. use Large\Zhengdada\Directory\Directory;
 2. Directory::mkdirs($dir = '' , $chmod = 0755)  
 $dir：要创建的目录;  
 $chmod：文件权限 默认:0755;  
+## 结束
 
-## License
 
+## 自定义 记录日志 方法
 
-## Contributing
-
-自定义 记录日志 方法
 1. use Large\Zhengdada\LargeLog;
 2. Logs::mkLogFile($dir, $log = '', $size = 3)  
 $dir: 存放目录;  
 $log：要记录的日志, 默认:空;  
 $size：记录多少M后切换另一个文件，默认:3M;  
+## 结束
 
-## License
-
-## Contributing 
-
-记录 sql 日志用法 。 
+## 事件监听 记录 sql 日志用法
 
 1. 只要在 app\Providers\EventServiceProvider.php 配置 事件监听就可以了  
  ```
@@ -50,6 +41,57 @@ protected $listen = [
     ],
 ];
 ```
-## License
+## 结束
+
+
+## 搜索条件格式化 复用方法
+
+1 . 控制器调用
+```$xslt
+use Large\Zhengdada\Search\Search;
+
+public function goodsBrowseData(Request $request){
+
+    list($limit , $where , $verify , $exclude) = Search::formatWehre($request);
+
+```  
+2 . 传入参数格式
+```$xslt
+$arr['parame']          // 要搜索的字段      
+$arr['condition']       // 搜索字段条件格式   
+$arr['excludeField']    // 不需要搜索的条件
+
+$arr['parame'] = [
+	'goods_name'	=>	'苹果',
+	'price'         =>	'3999',
+	'created_at'	=>	'2019-12-12 / 2019-12-29',
+]
+$arr['condition'] = [
+	'goods_name'	=>	'%*%',
+	'price'         =>	'>',
+	'created_at'	=>	'between',
+]
+$arr['excludeField'] = [
+	'created_at'	=>	'true'
+]
+
+或者下面格式
+
+parame :{
+    goods_name:'苹果'
+    ,price:'3999'
+    ,created_at:2019-12-12 / 2019-12-29
+},
+condition:{
+    goods_name:'%*%'
+    ,price:'>'
+    ,created_at:'between'
+},
+excludeField:{
+    created_at:true,
+}
+```
+## 结束
+
 
 MIT
---
+++ src/Search/Search.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020-7-17
+ * Time: 15:10
+ */
+
+namespace Large\Zhengdada\Search;
+
+use Large\Zhengdada\Functions;
+
+class Search extends Functions
+{
+
+    /****
+     * @param array $arr 查询条件数组
+     * $page       当前第几页
+     * $limit      一次查询多少条数据
+     * $parame     要进行搜索的字段
+     * $condition  字段搜索方式
+     * $excludeField  过滤不须要查询的字段
+     * @return array 返回一个数组
+     */
+    protected function formatWehre($arr = [])
+    {
+        $page           =   @$arr['page'] ? $arr['page'] : 1;// 当前第几页
+        $limit          =   @$arr['limit'] ? $arr['limit'] : 10;// 一次查询多少条数据
+        $parame         =   !empty(@$arr['parame']) ? $arr['parame'] : [];// 要进行搜索的字段
+        $condition      =   !empty($arr['condition']) ? $arr['condition'] : [];// 字段搜索方式
+        $excludeField   =   @$arr['exclude_field'] ? array_keys($arr['exclude_field']) : [];// 过滤不须要查询的字段
+
+        $where          = [];        // 在where查询的条件
+        $exclude        = [];      // 不在where查询的条件，自行处理
+        $verify         = false;    // 是否查询，如果为true 进行where查询
+
+        // 循环要搜索的字段。
+        foreach ($parame as $k=>$v){
+            if (in_array($k , $excludeField)){
+                $exclude[$k] = $v;
+                continue;
+            }
+
+            // 如果有值，就查询
+            if (empty($v)){
+                continue;
+            }else{
+                $verify = true;
+            }
+
+            $condition = !empty(@$condition[$k]) ? $condition[$k] : '%*%';
+            switch (strtolower($condition)){
+                case '=':
+                    $where[] = [$k , '=' , $v];
+                    break;
+                case '>':
+                    $where[] = [$k , '>' , $v];
+                    break;
+                case '>=':
+                    $where[] = [$k , '>=' , $v];
+                    break;
+                case '<':
+                    $where[] = [$k , '<' , $v];
+                    break;
+                case '<=':
+                    $where[] = [$k , '<=' , $v];
+                    break;
+                case '%*%':
+                    $where[] = [$k , 'LIKE' , "%{$v}%"];
+                    break;
+                case '*%':
+                    $where[] = [$k , 'LIKE' , "{$v}%"];
+                    break;
+                case '%*':
+                    $where[] = [$k , 'LIKE' , "%{$v}"];
+                    break;
+                case 'between':
+                    list($t1,$t2) = explode('/' , $v);
+                    $where[] = [$k , 'between' , [$t1 , $t2.' 23:59:59']];
+                    break;
+            }
+        }
+        return [$limit , $where , $verify , $exclude ,$page];
+    }
+
+}
