73f96584ac56ed6d23b01709176016772bb6bf18
Your Name <you@example.com>
2021-03-21 22:44:25 +0800

    修改一些BUG

--- routes/large.php
+++ routes/large.php
@@ -2,26 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 use Large\Zhengdada\Controllers\TestController;
+use Illuminate\Support\Facades\Cookie;
 
 
 
-Route::get('test2', function (){
+Route::group(["middleware" => ["web"]], function () {
+    Route::get('test2', function (){
 
-    dd('测试一下');
+        dd('测试一下', Cookie::get('language'));
+    });
+
+    // 设置语言 路由设置
+    Route::get('lang/{lang}', function($lang){
+        $newLang = strtolower($lang);
+        if (in_array($newLang , Config('app.locales'))){
+            Cookie::queue('language' , $newLang);
+        }
+        return redirect()->back();
+    });
+
+
+    Route::get('hhhh', [TestController::class, "index"])->name('hhhh');
+
+
+
 });
-
-
-// 设置语言 路由设置
-Route::get('lang/{lang}', function($lang){
-    $newLang = strtolower($lang);
-    if (in_array($newLang , \Config('app.locales'))){
-        Cookie::queue('language' , $newLang);
-    }
-    return redirect()->back();
-});
-
-
-Route::get('hhhh', 'TestController@index')->name('hhhh');
 
 
 
